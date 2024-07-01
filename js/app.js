f14c5f03386971b2e8330c46dc141860bdc6174a
Manuel Joaquín González <manujoaquingonzalez5@gmail.com>
2024-07-01 18:50:49 -0300

    le agregue un bucle y quite alerts adicionales

--- js/app.js
+++ js/app.js
@@ -21,7 +21,7 @@ function convertir() {
 
     let tasaCambioEth = 340000.00;
 
-    let opcion = prompt('¿Queres operación desea simular? \n 1) Convertir AR$ a BTC \n 2) Convertir BTC a AR$ \n 3) Convertir AR$ a ETH \n 4) Convertir ETH a AR$ \n 5) Salir \n Ingrese 1, 2, 3 o 4 para avanzar.');
+    let opcion = prompt('¿Que operación desea simular? \n 1) Convertir AR$ a BTC \n 2) Convertir BTC a AR$ \n 3) Convertir AR$ a ETH \n 4) Convertir ETH a AR$ \n 5) Salir \n Ingrese 1, 2, 3 o 4 para avanzar.');
 
     if (opcion === '1') {
         let pesos = parseFloat(prompt('Ingrese la cantidad de AR$ que desea convertir en BTC (ingrese el valor sin puntos ni comas):'))
@@ -30,7 +30,6 @@ function convertir() {
         } else {
             let bitcoin = pesosABitcoin(pesos, tasaCambioBtc);
             alert(`El equivalente en bitcoin de AR$${formatearDecimal(pesos)} es ₿${formatearDecimal(bitcoin)}`);
-            alert('Muchas gracias por usar nuestro simulador!');
         }
     } else if (opcion === '2') {
         let bitcoin = parseFloat(prompt('Ingrese la cantidad de BTC que desea convertir en AR$ (ingrese el valor sin puntos ni comas):'))
@@ -39,7 +38,6 @@ function convertir() {
         } else {
             let pesos = bitcoinAPesos(bitcoin, tasaCambioBtc);
             alert(`El equivalente en pesos argentinos de ₿${formatearDecimal(bitcoin)} es AR$${formatearDecimal(pesos)}`);
-            alert('Muchas gracias por usar nuestro simulador!');
         }
     } else if (opcion === '3') {
         let pesos = parseFloat(prompt('Ingrese la cantidad de AR$ que desea convertir en ETH (ingrese el valor sin puntos ni comas):'))
@@ -48,7 +46,6 @@ function convertir() {
         } else {
             let ethereum = pesosAEthereum(pesos, tasaCambioEth);
             alert(`El equivalente en ethereum de AR$${formatearDecimal(pesos)} es ETH ${formatearDecimal(ethereum)}`);
-            alert('Muchas gracias por usar nuestro simulador!');
         }
     } else if (opcion === '4') {
         let ethereum = parseFloat(prompt('Ingrese la cantidad de ETH que desea convertir en AR$ (ingrese el valor sin puntos ni comas):'))
@@ -57,7 +54,6 @@ function convertir() {
         } else {
             let pesos = ethereumAPesos(ethereum, tasaCambioEth);
             alert(`El equivalente en pesos argentinos de ETH ${formatearDecimal(ethereum)} es AR$${formatearDecimal(pesos)}`);
-            alert('Muchas gracias por usar nuestro simulador!');
         }
     } if (opcion === '5') {
     alert('Muchas gracias por usar nuestro simulador!');
@@ -65,5 +61,21 @@ function convertir() {
     else if (opcion !== '1' && opcion !== '2' && opcion !== '3' && opcion !== '4' && opcion !== '5') {
     alert('Por favor ingrese una opción correcta.');
     }
-}
+    while (true) {
+        repetir = prompt("¿Desea hacer otra simulación? Elija por 'si' o 'no'.").toLowerCase();
+        if (repetir === "si" || repetir === "no") {
+            break;
+        } else {
+            alert("Por favor, responde con 'si' o 'no'.");
+        }
+    }
+    while (repetir === "si") {
+        convertir();
+    }
+    while (repetir === "no") {
+        alert("Muchas gracias por usar nuestro simulador!");
+        break;
+    }
+} 
+
 convertir();
