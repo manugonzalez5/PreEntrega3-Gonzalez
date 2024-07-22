56807dc68aa738d1ec9565ecffac47b69871570f
Manuel Joaquín González <manujoaquingonzalez5@gmail.com>
2024-07-22 17:35:13 -0300

    ordene el codigo

--- js/main.js
+++ js/main.js
@@ -6,22 +6,22 @@ let menu_str = `
     Ingrese 3 para buscar productos.
     Ingrese 4 para incrementar todos los precios.
     Ingrese 5 para editar un producto.
-    Ingrese 0 para VOLVER o SALIR.
+    Ingrese 0 para SALIR.
 `;
 
 function solicitarDato(tipo, mensaje, sugerencia = "") {
 let dato;
 if (tipo == "numero") {
     do {
-    dato = parseFloat(prompt(mensaje, sugerencia));
+        dato = parseFloat(prompt(mensaje, sugerencia));
     } while (isNaN(dato) || dato < 0);
 } else if (tipo == "numeroEntero") {
     do {
-    dato = parseInt(prompt(mensaje, sugerencia));
+        dato = parseInt(prompt(mensaje, sugerencia));
     } while (isNaN(dato) || dato < 0);
 } else if (tipo == "cadena") {
     do {
-    dato = prompt(mensaje, sugerencia);
+        dato = prompt(mensaje, sugerencia);
     } while (dato == "");
 }
 return dato;
@@ -32,32 +32,30 @@ while (!salir) {
 let opcion = parseInt(prompt(menu_str));
 switch (opcion) {
     case 0:
-    alert(
-        "Hasta luego, muchas gracias. Para volver a ejecutar aprete la tecla F5"
-    );
-    salir = true;
-    break;
+        alert("Hasta luego, muchas gracias. Para volver a ejecutar recarga la pagina");
+        salir = true;
+        break;
     case 1:
-    mostrarTabla(productos);
-    break;
+        mostrarTabla(productos);
+        break;
     case 2:
-    sistema_stock.nuevoProducto();
-    mostrarTabla(productos);
-    break;
+        sistema_stock.nuevoProducto();
+        mostrarTabla(productos);
+        break;
     case 3:
-    filtrarProductos(productos);
-    break;
+        filtrarProductos(productos);
+        break;
     case 4:
-    incrementarPrecios(productos);
-    mostrarTabla(productos);
-    break;
+        incrementarPrecios(productos);
+        mostrarTabla(productos);
+        break;
     case 5:
-    producto = buscarId(productos);
-    editarProducto(producto);
-    mostrarTabla(productos);
-    break;
+        producto = buscarId(productos);
+        editarProducto(producto);
+        mostrarTabla(productos);
+        break;
     default:
-    alert("Opción inexistente. Intente nuevamente.");
-    break;
+        alert("Opción inexistente. Intente nuevamente.");
+        break;
 }
 }
