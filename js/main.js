67acd6a0dadc4bd87d901b3526e080b2e74e6ee2
Manuel Joaquín González <manujoaquingonzalez5@gmail.com>
2024-08-01 16:29:34 -0300

    agregue html, css y edite funciones js

--- js/main.js
+++ js/main.js
@@ -1,53 +1,181 @@
-let sistema_stock = new Sistema(productos);
 
-let menu_str = `
-    Ingrese 1 para listar todos los productos.
-    Ingrese 2 para agregar producto nuevo.
-    Ingrese 3 para buscar productos.
-    Ingrese 4 para incrementar todos los precios.
-    Ingrese 5 para editar un producto.
-    Ingrese 0 para SALIR.
-`;
+
+function guardarProductos() {
+    localStorage.setItem('productos', JSON.stringify(productos));
+}
+
+let listProductos = document.querySelector("#listar-productos");
+let agregarProducto = document.querySelector("#agregar-producto");
+let buscarProducto = document.querySelector("#buscar-producto");
+let incrementarPrecioss = document.querySelector("#incrementar-producto");
+let editarProductoss  = document.querySelector("#editar-producto");
+
+
+
+
+
+listProductos.addEventListener("click", () => {
+    let formulario = document.querySelector("#form");
+    formulario.innerHTML = '';
+mostrarTabla(productos);
+});
+
+let alerta = document.querySelector("#alert");
+
+
+agregarProducto.addEventListener("click", () => {
+    
+let formulario = document.querySelector("#form");
+
+formulario.innerHTML = `<input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1 md:col-span-2" placeholder="Nombre de producto" type="text" id="nombre" />
+    <input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Rubro de producto" type="text" id="rubro" />
+    <input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Precio de producto" type="number" id="precio" />
+    <button type="submit" class="p-2 text-center bg-cyan-600 text-white col-start-1 md:col-start-2 hover:bg-cyan-900 self-end" id="submitProducto">Agregar Producto</button>`;
+
+document.querySelector("#submitProducto").addEventListener("click", (event) => {
+    event.preventDefault();
+
+    let nombre = document.querySelector("#nombre").value;
+    let precio = document.querySelector("#precio").value;
+    let rubro = document.querySelector("#rubro").value;
+    console.log(nombre, precio, rubro);
+    if (nombre && rubro && precio) {
+        let productoExiste = productos.some(
+        (producto) => producto.nombre === nombre);
+        if (!productoExiste) {
+        let nuevoProducto = new Producto(nombre, parseFloat(precio), rubro);
+        productos.push(nuevoProducto);
+        guardarProductos();
+        mostrarTabla(productos);
+        formulario.reset();
+        alerta.innerHTML = `<p class="w-full h-full bg-green-300 p-4">${nombre} agregado con exito!</p>`;
+        } else {
+        alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">${nombre} ya existe!</p>`;
+        }
+    } else {
+        alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">Por favor completa los campos</p>`;
+    }
+    setTimeout(() => {
+        alerta.innerHTML = '';
+    }, 3000);
+    });
+mostrarTabla(productos);
+});
+
+
+buscarProducto.addEventListener("click", () => {
+    
+let formulario = document.querySelector("#form");
+
+formulario.innerHTML = `<input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Nombre de producto" type="text" id="nombre" />
+    <button type="submit" class="p-2 text-center bg-cyan-600 text-white col-start-1 md:col-start-2 hover:bg-cyan-900 self-end" id="submitProducto">Buscar Producto</button>`;
+
+document.querySelector("#submitProducto").addEventListener("click", (event) => {
+    event.preventDefault(); 
+
+    let nombre = document.querySelector("#nombre").value;
+    console.log(nombre);
+    if (nombre) {
+        let productoExiste = productos.some(
+            (producto) => producto.nombre === nombre);
+        if (!productoExiste) {
+            alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">${nombre} no existe!</p>`;
+        } else {
+            alerta.innerHTML = `<p class="w-full h-full bg-green-300 p-4">${nombre} existe!</p>`;
+        }
+    } else {
+        alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">Por favor completa el campo "Nombre de producto"</p>`;
+    }
+    setTimeout(() => {
+        alerta.innerHTML = '';
+    }, 3000);
+    });
+    mostrarTabla(productos);
+});
+
+
+incrementarPrecioss.addEventListener("click", () => {
+    let formulario = document.querySelector("#form");
+
+    formulario.innerHTML = `
+        <input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Porcentaje de aumento" type="number" id="porcentaje" />
+        <button type="submit" class="p-2 text-center bg-cyan-600 text-white col-start-1 md:col-start-2 hover:bg-cyan-900 self-end" id="submitIncremento">Incrementar Precios</button>`;
+
+    document.querySelector("#submitIncremento").addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let porcentaje = parseFloat(document.querySelector("#porcentaje").value);
+        if (!isNaN(porcentaje)) {
+            productos.forEach((producto) => {
+                producto.precio += (producto.precio * porcentaje) / 100;
+            });
+            guardarProductos();
+            mostrarTabla(productos);
+        } else {
+            alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">Por favor ingresa un porcentaje válido</p>`;
+        }
+        setTimeout(() => {
+            alerta.innerHTML = '';
+        }, 3000);
+    });
+    mostrarTabla(productos);
+});
+
+
+editarProductoss.addEventListener("click", () => {
+    let formulario = document.querySelector("#form");
+    formulario.innerHTML = `<input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1 md:col-span-2" placeholder="Nombre de producto" type="text" id="nombre" />
+    <input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Rubro de producto" type="text" id="rubro" />
+    <input class="p-2 border border-cyan-600 outline-none focus:ring-cyan-600 col-span-1" placeholder="Precio de producto" type="number" id="precio" />
+    <button type="submit" class="p-2 text-center bg-cyan-600 text-white col-start-1 md:col-start-2 hover:bg-cyan-900 self-end" id="submitProducto">Editar Producto</button>`;
+
+    document.querySelector("#submitProducto").addEventListener("click", (event) => {
+        event.preventDefault();
+
+        let nombre = document.querySelector("#nombre").value;
+        let rubro = document.querySelector("#rubro").value;
+        let precio = document.querySelector("#precio").value;
+        if (nombre && rubro && precio) {
+            let productoExiste = productos.some(
+            (producto) => producto.nombre === nombre);
+            if (!productoExiste) { 
+                alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">${nombre} no existe!</p>`;
+            } else {
+                let index = productos.findIndex((producto) => producto.nombre === nombre);
+                productos[index].rubro = rubro;
+                productos[index].precio = parseFloat(precio);
+                guardarProductos();
+                mostrarTabla(productos);
+                alerta.innerHTML = `<p class="w-full h-full bg-green-300 p-4">${nombre} editado con exito!</p>`;
+            }
+        } else {
+            alerta.innerHTML = `<p class="w-full h-full bg-red-300 p-4">Por favor completa los campos</p>`;
+        }
+        setTimeout(() => {
+            alerta.innerHTML = '';
+        }, 3000);
+        });
+        mostrarTabla(productos);
+    });
 
 function solicitarDato(tipo, mensaje, sugerencia = "") {
 let dato;
 if (tipo == "numero") {
     do {
-        dato = parseFloat(prompt(mensaje, sugerencia));
+    dato = parseFloat(prompt(mensaje, sugerencia));
     } while (isNaN(dato) || dato < 0);
 } else if (tipo == "numeroEntero") {
     do {
-        dato = parseInt(prompt(mensaje, sugerencia));
+    dato = parseInt(prompt(mensaje, sugerencia));
     } while (isNaN(dato) || dato < 0);
 } else if (tipo == "cadena") {
     do {
-        dato = prompt(mensaje, sugerencia);
+    dato = prompt(mensaje, sugerencia);
     } while (dato == "");
 }
 return dato;
-}
+}   
 
-while (true) {
-    let opcion = Number(prompt(menu_str));
-    
-    if (opcion === 0) {
-        alert("Hasta luego, muchas gracias. Para volver a ejecutar recarga la pagina");
-        break; 
-    } else if (opcion === 1) {
-        mostrarTabla(productos);
-    } else if (opcion === 2) {
-        sistema_stock.nuevoProducto();
-        mostrarTabla(productos);
-    } else if (opcion === 3) {
-        filtrarProductos(productos);
-    } else if (opcion === 4) {
-        incrementarPrecios(productos);
-        mostrarTabla(productos);
-    } else if (opcion === 5) {
-        const producto = buscarId(productos);
-        editarProducto(producto);
-        mostrarTabla(productos);
-    } else {
-        alert("Opción inexistente. Intente nuevamente.");
-    }
-}
+
+
+
--- js/sistema.js
+++ js/sistema.js
@@ -1,83 +1,66 @@
 class Sistema {
-constructor(productos) {
-    this.productos = productos;
-}
+    constructor(productos) {
+        this.productos = productos;
+    }
 
-nuevoProducto() {
-    const nombre = solicitarDato("cadena", "Ingrese el nombre del producto");
-    const rubro = solicitarDato("cadena", "Ingrese el rubro del producto");
-    const precio = solicitarDato(
-    "numeroEntero",
-    "Ingrese el precio del producto"
-    );
-    this.productos.push(new Producto(nombre, precio, rubro));
-}
-}
-
-function buscarId(productos) {
-const id = solicitarDato("cadena", "Ingrese el ID del producto");
-return productos.find((producto) => producto.id == id);
-}
-
-function textoMenu(opciones) {
-return opciones.join("");
+    nuevoProducto() {
+        const nombre = solicitarDato("cadena", "Ingrese el nombre del producto");
+        const rubro = solicitarDato("cadena", "Ingrese el rubro del producto");
+        const precio = solicitarDato(
+            "numeroEntero",
+            "Ingrese el precio del producto"
+        );
+        this.productos.push(new Producto(nombre, precio, rubro));
+    }
 }
 
 function mostrarTabla(datos) {
-console.clear();
-console.table(datos);
-}
+    let tablita = document.querySelector('#tabla-productos')
 
-function incrementarPrecios(productos) {
-let porcentaje = solicitarDato(
-    "numero",
-    "Ingrese el porcentaje de aumento a aplicar o 0 para cancelar"
-);
-productos.forEach((producto) => {
-    producto.precio = (producto.precio * porcentaje) / 100 + producto.precio;
-});
-mostrarTabla(productos);
-}
+    tablita.innerHTML = `
+<div class="relative overflow-x-auto w-full md:w-[600px] lg:w-[900px]">
+    <table class="w-full text-sm text-left rtl:text-right text-white">
+        <thead class="text-xs  uppercase bg-cyan-900 ">
+            <tr>
+                <th scope="col" class="px-6 py-3">
+                    Nombre
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Precio
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Rubro
+                </th>
+                <th scope="col" class="px-6 py-3">
+                    Stock
+                </th>
+            </tr>
+        </thead>
+        <tbody>
+        </tbody>
+    </table>
+</div>
+    `
+    let tbody = tablita.querySelector('tbody');
 
-function filtrarProductos(productos) {
-let busqueda = solicitarDato(
-    "cadena",
-    "Ingrese la palabra o frase, se buscará en nombre"
-);
-const result = productos.filter(
-    (producto) =>
-    producto.nombre.toLowerCase().indexOf(busqueda.toLowerCase()) != -1 ||
-    (producto.rubro &&
-        producto.rubro.toLowerCase().indexOf(busqueda.toLowerCase()) != -1)
-);
-mostrarTabla(result);
-if (result.length == 0) {
-    alert(`Su búsqueda ${busqueda} arrojó 0 resultados`);
-}
-console.log(
-    `Se encontraron ${result.length} resultados para su búsqueda ${busqueda}`
-);
-}
-
-function editarProducto(producto) {
-producto = buscarId(productos);
-if (!producto) {
-    alert("Producto no encontrado");
-    return;
-}
-producto.nombre = solicitarDato(
-    "cadena",
-    "Ingrese el nombre del producto",
-    producto.nombre
-);
-producto.precio = solicitarDato(
-    "numeroEntero",
-    "Ingrese el precio del producto",
-    producto.precio
-);
-producto.stock = solicitarDato(
-    "numeroEntero",
-    "Ingrese el stock disponible del producto",
-    producto.stock
-);
+    datos.forEach((producto => {
+        tbody.innerHTML += `
+        <tr class="bg-cyan-700 border-b even:bg-cyan-950" id='${producto.nombre}-${producto.id}'>
+                <th scope="row" class="px-6 py-4 font-medium whitespace-nowrap text-white">
+                    ${producto.nombre}	
+                </th>
+                <td class="px-6 py-4">
+                    ${producto.precio}
+                </td>
+                <td class="px-6 py-4">
+                    ${producto.rubro}
+                </td>
+                <td class="px-6 py-4">
+                    ${producto.stock}
+                </td>
+            </tr>
+        `
+    }))
+    console.clear();
+    console.table(datos);
 }
